1a72859d0d557886b0cd34141efc11590e702f63
RevolveStrm <revolvebattlerage@gmail.com>
2024-11-11 13:27:29 +0200

    add logger middleware

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from 'src/auth/auth.module';
+import { LoggerMiddleware } from 'src/logger/logger.middleware';
 import { UserModule } from 'src/user/user.module';
 import { TasksModule } from '../tasks/tasks.module';
 
@@ -9,4 +10,8 @@ import { TasksModule } from '../tasks/tasks.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
