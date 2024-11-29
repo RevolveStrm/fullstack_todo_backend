a052e000a222fa98621a5618c72eca5df9e661fa
RevolveStrm <revolvebattlerage@gmail.com>
2024-11-29 16:08:12 +0200

    feat(types): update global and JWT payload types

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,7 +1,9 @@
-import { Request } from 'express';
+import { Request, User } from 'express';
 import { JwtPayload } from './jwt-payload';
 
 declare global {
+  interface User extends JwtPayload {}
+
   interface Request {
     user?: JwtPayload;
   }
